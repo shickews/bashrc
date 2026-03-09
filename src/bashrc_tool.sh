f88f63e01ecf55271febe5a42a8f216d326d6e35
shickews <nwcnhetj4083@outlook.com>
2026-03-09 13:53:12 +0800

    feat: add bashrc tool script

---
+++ src/bashrc_tool.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# A simple bashrc manager script
+
+function load_bashrc() {
+    source ~/.bashrc
+}
+
+function customize_bash() {
+    echo "Customizing bash settings..."
+    # Add customization logic here
+}
+
+# Main logic
+load_bashrc
+customize_bash
